85e117a6ed8da50275916cb2bc6e00013fac4d52
Alex Pilugin <pilugin@gmail.com>
2017-02-27 00:55:39 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+var express = require('express')
+var path = require('path')
+var React = require('react')
+var ReactDOM = require('react-dom')
+var app = express()
+
+app.use(express.static(path.join(__dirname+'/public')));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+  });
+
+var config = {};
+
+config.PORT = 3030;
+
+var server = app.listen(config.PORT, () => {
+  console.log('Server started on 3030 port at:' + Date());
+});
---
+++ public/app-01.jsx
@@ -0,0 +1,6 @@
+const App_01 = () => <h2>Hello from App_01</h2>
+
+ReactDOM.render(
+  <App_01 />,
+  document.getElementById('app-01')
+);
